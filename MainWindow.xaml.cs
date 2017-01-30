067e31890638d8ef5bb9431a0332c4c3321bb3fe
cliversoft <sergey.stoyan@gmail.com>
2017-01-30 04:11:29 +0200

    .

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace Cliver.CefSharpController
 
             browser.Loaded += Browser_Loaded;
             browser.LoadingStateChanged += Browser_LoadingStateChanged;
+            browser.RequestHandler = new RequestHandler();
 
 
 
@@ -45,6 +46,80 @@ namespace Cliver.CefSharpController
             //jsDriver.ExecuteScript(highlightJavascript, new object[] { element });
         }
         public static MainWindow This { get; private set; }
+
+        class RequestHandler : IRequestHandler
+        {
+            public bool GetAuthCredentials(IWebBrowser browserControl, IBrowser browser, IFrame frame, bool isProxy, string host, int port, string realm, string scheme, IAuthCallback callback)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IResponseFilter GetResourceResponseFilter(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool OnBeforeBrowse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, bool isRedirect)
+            {
+                return !Block;
+            }
+            public bool Block = false;
+
+            public CefReturnValue OnBeforeResourceLoad(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IRequestCallback callback)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool OnCertificateError(IWebBrowser browserControl, IBrowser browser, CefErrorCode errorCode, string requestUrl, ISslInfo sslInfo, IRequestCallback callback)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool OnOpenUrlFromTab(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl, WindowOpenDisposition targetDisposition, bool userGesture)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnPluginCrashed(IWebBrowser browserControl, IBrowser browser, string pluginPath)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool OnProtocolExecution(IWebBrowser browserControl, IBrowser browser, string url)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool OnQuotaRequest(IWebBrowser browserControl, IBrowser browser, string originUrl, long newSize, IRequestCallback callback)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnRenderProcessTerminated(IWebBrowser browserControl, IBrowser browser, CefTerminationStatus status)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnRenderViewReady(IWebBrowser browserControl, IBrowser browser)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnResourceLoadComplete(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response, UrlRequestStatus status, long receivedContentLength)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnResourceRedirect(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, ref string newUrl)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool OnResourceResponse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response)
+            {
+                throw new NotImplementedException();
+            }
+        }
 
         private void Browser_Loaded(object sender, RoutedEventArgs e)
         {
